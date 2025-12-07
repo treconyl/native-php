72240b5f08f38501df0db155d1b7e866d2c5351a
Treconyl <treconyl@gmail.com>
2025-12-07 14:31:13 +0700

    PreventRegularBrowserAccess

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Native\Desktop\Http\Middleware\PreventRegularBrowserAccess;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Cho phép truy cập từ trình duyệt ngoài (bỏ chặn của NativePHP).
+        $middleware->web(remove: [
+            PreventRegularBrowserAccess::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
